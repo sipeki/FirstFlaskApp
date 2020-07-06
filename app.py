880bae39986e49c2c478b32888fb20a140afef06
sipeki <sipeki@kindlen.eu>
2020-07-06 16:44:58 +0100

    homepage

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello Everyone!!!!!'
+    return render_template('homepage.html', title="Homepage")
 
 
 if __name__ == '__main__':
